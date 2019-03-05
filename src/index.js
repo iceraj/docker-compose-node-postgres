666d3251121ecc67b988c89683cefc1020e7faf5
Ivica Ceraj <iceraj@gmail.com>
2019-03-04 22:54:25 -0500

    use basic sequilize

--- src/index.js
+++ src/index.js
@@ -1,13 +1,27 @@
 import express from "express";
-import PgAsync, {SQL} from 'pg-async';
+import Sequelize from 'sequelize';
+// const Sequelize = require('sequelize');
 
 const app = express();
 console.info("DBURL: %s", process.env['DBURL'])
-const pgAsync = new PgAsync({connectionString: process.env['DBURL']});
 const asyncHandler = require('express-async-handler')
 
+const sequelize = new Sequelize(process.env['DBURL']);
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.info('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+
+
+
 app.get("/",  asyncHandler(async (req, res,next) =>  {
-  const result = await pgAsync.query(SQL`select now() as time`);
+  const result = await sequelize.query("SELECT now() as now", { type: sequelize.QueryTypes.SELECT})
   res.send(`result=<pre>${JSON.stringify(result)}</pre>`);
 }));
 
